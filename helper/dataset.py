0620019fcae0c8b0963fedf51717bfb4fafdec6a
Riza Rizky <42672299+zaRizk7@users.noreply.github.com>
2023-01-09 16:58:28 +0000

    Add random apply for blur

--- helper/dataset.py
+++ helper/dataset.py
@@ -14,7 +14,7 @@ DEFAULT_TRANSFORM = T.Compose(
         T.RandomVerticalFlip(),
         T.RandomHorizontalFlip(),
         T.RandAugment(),
-        T.GaussianBlur((224 // 10 + 1, 224 // 10 + 1)),
+        T.RandomApply([T.GaussianBlur((224 // 10 - 1, 224 // 10 - 1))]),
         T.ToTensor(),
     ]
 )
